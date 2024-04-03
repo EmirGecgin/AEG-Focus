e071fb7e598c33c5cb9e2bd77beaeda7a62ff9f3
EmirGecgin\Emir Gecgin <gecginemir@gmail.com>
2024-04-03 04:04:46 +0300

    +To do list is added. +Update is done.

--- functions.js
+++ functions.js
@@ -3,6 +3,7 @@ function incrementNumberOfLooking() {
     let incrementNumberOfLook = Number(defaultNumberOfLook.textContent);
     incrementNumberOfLook += 1;
     defaultNumberOfLook.textContent = incrementNumberOfLook;
+    localStorage.setItem("numberOfLooking", incrementNumberOfLook);
     if (incrementNumberOfLook >= 1) {
         defaultNumberOfLook.style.color = "green";
     }
@@ -13,11 +14,20 @@ function resetTheNumberOfLooking() {
     let reset = Number(currentNumberOfLook.textContent);
     reset = 0;
     currentNumberOfLook.textContent = reset;
+    localStorage.setItem("numberOfLooking", 0);
     if (currentNumberOfLook.textContent == 0) {
         currentNumberOfLook.style.color = "white";
     }
 }
 
+const storedNumber = localStorage.getItem("numberOfLooking");
+if (storedNumber) {
+    let number = document.getElementById("looking-number")
+    number.textContent = storedNumber;
+    if (storedNumber >= 1) {
+        number.style.color = "green";
+    }
+}
 
 function playVoice(type0fVoice) {
     let voice = document.getElementById("voice" + type0fVoice);
@@ -60,10 +70,69 @@ let voice1 = document.getElementById("voicebird");
 let voice2 = document.getElementById("voicerain");
 let voice3 = document.getElementById("voicewind");
 let voice4 = document.getElementById("voicesea");
+let voiceIcon = document.getElementById("voiceIcon");
+
+
 
 function volumeAdjustment() {
     voice1.volume = range.value / 100;
     voice2.volume = range.value / 100;
     voice3.volume = range.value / 100;
     voice4.volume = range.value / 100;
+    if (voice1.volume === 0) {
+        voiceIcon.classList.remove("fa-volume-low");
+        voiceIcon.classList.add("fa-volume-xmark");
+    } else {
+        voiceIcon.classList.add("fa-volume-low");
+        voiceIcon.classList.remove("fa-volume-xmark");
+    }
+}
+
+const taskButton = document.getElementById("task-button");
+const taskInput = document.getElementById("input-task");
+const taskList = document.getElementById("list-for-task");
+
+getItemsFromLocale();
+
+function addTask() {
+    const task = taskInput.value.trim();
+    if (task) {
+        createTaskList(task);
+        taskInput.value = "";
+        saveTasksInLocale();
+    } else {
+        alert("You must write something!");
+    }
+}
+
+taskButton.addEventListener("click", addTask);
+
+function createTaskList(tasks) {
+    const taskLI = document.createElement("li");
+    taskLI.textContent = tasks;
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "delete-button ";
+    taskLI.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", function() {
+        taskList.removeChild(taskLI);
+        saveTasksInLocale();
+    });
+
+
+    taskList.appendChild(taskLI);
+}
+
+function saveTasksInLocale() {
+    const taskItems = [];
+    taskList.querySelectorAll("li").forEach(function(items) {
+        taskItems.push(items.textContent.replace("Delete", "").trim());
+    });
+    localStorage.setItem("taskItems", JSON.stringify(taskItems));
+}
+
+function getItemsFromLocale() {
+    const getItems = JSON.parse(localStorage.getItem("taskItems"));
+    getItems.forEach(createTaskList);
 }
